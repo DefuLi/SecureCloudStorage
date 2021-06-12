5a31201a83496200af19674bbf3362d7d5e96e1a
shijichen-wilf <57850971+shijichen-wilf@users.noreply.github.com>
2021-06-12 23:24:06 +0800

    1. 数据库

---
+++ iotlab.sql
@@ -0,0 +1,25 @@
+﻿# Host: localhost  (Version: 5.5.27)
+# Date: 2021-06-12 23:15:52
+# Generator: MySQL-Front 5.3  (Build 4.214)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+INSERT INTO `user` VALUES (1,'admin','123456','admin','https://avatars0.githubusercontent.com/u/20942571?s=460&v=4');
