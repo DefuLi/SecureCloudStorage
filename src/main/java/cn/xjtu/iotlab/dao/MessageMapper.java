e7f4d0c6886c9b5df2badf15b1170007b2b95451
lidefu <defuli.go@qq.com>
2021-06-22 09:28:48 +0800

    新增消息处理线程池

---
+++ src/main/java/cn/xjtu/iotlab/dao/MessageMapper.java
@@ -0,0 +1,10 @@
+package cn.xjtu.iotlab.dao;
+
+/**
+ * 消息模块Dao层
+ *
+ * @author Defu Li
+ * @date 2021/6/22 8:49
+ */
+public interface MessageMapper {
+}
---
+++ src/main/java/cn/xjtu/iotlab/threads/MessageThreads.java
@@ -0,0 +1,31 @@
+package cn.xjtu.iotlab.threads;
+
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 消息模块线程池
+ *
+ * @author Defu Li
+ * @date 2021/6/22 8:43
+ */
+@Component
+public class MessageThreads {
+    public static final ExecutorService executorService = new ThreadPoolExecutor(
+            5, 10, 60L,
+            TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(5));
+
+    private static final MessageThreads instance = new MessageThreads();
+
+    private MessageThreads() {
+    }
+
+    public static final MessageThreads getInstance() {
+        return instance;
+    }
+
+}
