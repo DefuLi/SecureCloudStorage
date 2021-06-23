7a99842341c9e2d609c57fcade0703717c6c4a3d
wuao <1091237126@qq.com>
2021-06-23 17:40:28 +0800

    文件加密

--- src/main/java/cn/xjtu/iotlab/controller/BFEncDecController.java
+++ src/main/java/cn/xjtu/iotlab/controller/BFEncDecController.java
@@ -1,30 +1,38 @@
 package cn.xjtu.iotlab.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import cn.xjtu.iotlab.utils.encdec.TestBF;
+import org.apache.commons.io.FileUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * BF 文件加解密模块
   */
-@RequestMapping("BF")
+@RestController
+@RequestMapping("bf")
 public class BFEncDecController {
     //文件加密
     @ResponseBody
-    @RequestMapping(value = "/BFEncrypt", method = RequestMethod.POST)
-    public Object BFEncrypt(@RequestParam("file") File file ){
-
-
+    @RequestMapping(value = "/bfencrypt", method = RequestMethod.POST)
+    public int BFEncrypt(@RequestParam("file") MultipartFile multipartFile , @RequestParam("username") String username) throws IOException {
+        File file = new File(multipartFile.getOriginalFilename());
+        FileUtils.copyInputStreamToFile(multipartFile.getInputStream(), file);
+        TestBF bf = new TestBF();
+        boolean signal= bf.filebfencrpt(file,username);
         //前端
         //保存
         //上传
         //取消
-        return "success";
+        if(signal == true)
+        return 1;
+        else{
+            return 0;
+        }
     }
 
     //文件解密
